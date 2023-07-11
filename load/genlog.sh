07df83706629b81b5277714b64ee39691380fe92
Tangogow <tangogow@gmail.com>
2023-07-11 12:44:37 +0200

    indent & logpath

--- load/genlog.sh
+++ load/genlog.sh
@@ -21,53 +21,55 @@ log_path=$4
 logs_generated=0
 
 if [ -z "$log_path" ]; then
-  $log_path="/var/log/messages"
+  log_path="/var/log/messages"
 fi
 if [ ! -f "$log_path" ]; then
   touch $log_path
 fi
+echo "*.* $log_path" > /etc/rsyslog.d/logger.conf
 logger -f $log_path
+service rsyslog start || rsyslogd
 
 function report {
-    duration=$(($end_time - $start_time))
-    duration_secs=$duration
-    if [ $duration -lt 60 ]; then
-        duration=$duration" secs"
-    elif [ $duration -lt 3600 ]; then
-        duration=$(($duration / 60))" mins"
-    else
-        duration=$(($duration / 3600))" hours"
-    fi
-    estimated_duration=$(($logs_generated / $logs_per_second))
-    real_logs_per_second=$(($logs_generated / $duration_secs))
-    total_size=$(($logs_generated * $log_size))
-    total_size_bytes=$total_size
-    if [ $total_size -lt 1024 ]; then
-        total_size=$total_size" B"
-    elif [ $total_size -lt 1048576 ]; then
-        total_size=$(($total_size / 1024))" KB"
-    elif [ $total_size -lt 1073741824 ]; then
-        total_size=$(($total_size / 1048576))" MB"
-    else
-        total_size=$(($total_size / 1073741824))" GB"
-    fi
+  duration=$(($end_time - $start_time))
+  duration_secs=$duration
+  if [ $duration -lt 60 ]; then
+    duration=$duration" secs"
+  elif [ $duration -lt 3600 ]; then
+    duration=$(($duration / 60))" mins"
+  else
+    duration=$(($duration / 3600))" hours"
+  fi
+  estimated_duration=$(($logs_generated / $logs_per_second))
+  real_logs_per_second=$(($logs_generated / $duration_secs))
+  total_size=$(($logs_generated * $log_size))
+  total_size_bytes=$total_size
+  if [ $total_size -lt 1024 ]; then
+    total_size=$total_size" B"
+  elif [ $total_size -lt 1048576 ]; then
+    total_size=$(($total_size / 1024))" KB"
+  elif [ $total_size -lt 1073741824 ]; then
+    total_size=$(($total_size / 1048576))" MB"
+  else
+    total_size=$(($total_size / 1073741824))" GB"
+  fi
 
-    echo "===FAKE LOGS REPORT==="
-    echo "Logs generated     " $logs_generated
-    echo "Wanted logs/s      " $logs_per_second
-    echo "Real logs/s        " $real_logs_per_second
-    echo "Total size         " $total_size
-    echo "Estimated Duration " $estimated_duration
-    echo "Real Duration      " $duration
+  echo "===FAKE LOGS REPORT==="
+  echo "Logs generated     " $logs_generated
+  echo "Wanted logs/s      " $logs_per_second
+  echo "Real logs/s        " $real_logs_per_second
+  echo "Total size         " $total_size
+  echo "Estimated Duration " $estimated_duration
+  echo "Real Duration      " $duration
 
-    result="$logs_generated,$real_logs_per_second,$total_size_bytes,$duration_secs"
-    echo $result > /var/log/$CONTAINER_NAME.log
-    exit 0
+  result="$logs_generated,$real_logs_per_second,$total_size_bytes,$duration_secs"
+  echo $result > /var/log/$CONTAINER_NAME.log
+  exit 0
 }
 
 function ctrl_c {
-    end_time=$(date +%s)
-    report
+  end_time=$(date +%s)
+  report
 }
 
 start_time=$(date +%s)
@@ -93,13 +95,13 @@ while [[ "$logs_generated" -lt "$number_of_logs" ]]; do
   generate_log_entry
   logs_generated=$((logs_generated + 1))
   logs_per_second_count=$((logs_per_second_count + 1))
-  
+
   if [[ "$logs_per_second_count" -eq "$logs_per_second" ]]; then
     echo "Generated $logs_per_second_count logs"
     logs_per_second_count=0
   fi
   echo "Logs/s: " `awk -v d1="$(date --date='-1 second' +'%b %d %H:%M:%S')" -v d2="$(date +'%b %d %H:%M:%S')" \
-'$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/syslog | wc -l`
+'$0 > d1 && $0 < d2 || $0 ~ d2' $log_path | wc -l`
   sleep "$sleep_duration"
 done
 
