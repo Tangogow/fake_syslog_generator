64a0fd757b00b8ba585b29c6eb5376c6e1d4efbc
Tangogow <tangogow@gmail.com>
2023-07-20 15:46:18 +0200

    fix param genlog

--- load/genlog.sh
+++ load/genlog.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [[ $# -lt 3 || $# -gt 4 ]]; then
+if [[ $# -lt 4 || $# -gt 5 ]]; then
   echo -e "Usage: $0 <number_of_logs> <logs_per_second> <log_size> [<log_path>]\n"
   echo "You may be restricted by the number of logs per second, depending on your OS and disk IO's"
   echo "logs_per_second: in seconds (minimum 1)"
   echo "log_size: in bytes for each log entry"
   echo "log_path: the path of your log file. By default use /var/log/messages"
+  echo "remote_server: remote syslog server ip with port. ie: 192.168.0.1:514 (optional)"
   exit 1
 fi
 
@@ -22,7 +23,7 @@ logs_generated=0
 if [ ! -f "$log_path" ]; then
   touch $log_path
 fi
-if [ -z "$remote_server" ]; then
+if [ -z "$remote_server" ]; then # copying FAKE logs to the provided path
   echo -e "if \$programname == 'FAKE' then $log_path\n& stop" > /etc/rsyslog.d/01-logger.conf
 else
   echo -e "if \$programname == 'FAKE' then $log_path\nif \$programname == 'FAKE' then @@$remote_server& stop" > /etc/rsyslog.d/01-logger.conf
